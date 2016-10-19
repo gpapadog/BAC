a0ecd76ebf7107d07cc43ddaf87de4ee58f44f5a
gpapadog <gpapadogeorgou@fas.harvard.edu>
2016-10-19 15:23:28 -0400

    Build

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Toy data set.
+#' 
+#' A data set containing simulated data with a continuos exposure, outcome and
+#' covariates.
+#' 
+#'
+#' @format A matrix with 100 rows and 52 columns.
+#' \describe{
+#'   \item{Y}{Continuous outcome.}
+#'   \item{X}{Continuous exposure}
+#'   \item{D1, D2, ..., D50}{Possible confounders.}
+#' }
+"toyData"
